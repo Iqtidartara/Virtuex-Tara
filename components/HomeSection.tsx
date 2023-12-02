0ba6c037f899c564f9917ef5ce7406a8ba8a64a5
Iqtidartara <135112726+Iqtidartara@users.noreply.github.com>
2023-12-02 23:45:03 +0500

    with the new changes

--- components/HomeSection.tsx
+++ components/HomeSection.tsx
@@ -5,24 +5,24 @@ import { Button } from "@/components/ui/button";
 const HomeSection = () => {
   return (
     <div className="w-full pb-10">
-      <main className="mx-auto flex flex-col lg:flex-row lg:h-[80vh] px-6 py-4 md:py-12 max-w-7xl">
-        <div className="w-full lg:w-3/5 flex flex-col items-start gap-3 md:gap-8 text-center md:text-start">
-          <h3 className="text-blue-400 font-extrabold text-xs leading-normal tracking-tight uppercase md:mt-8 mt-3 mx-auto md:mx-0">
+      <main className="mx-auto flex flex-col lg:flex-row lg:h-[80vh] px-6 py-12 max-w-7xl">
+        <div className="w-full lg:w-3/5 flex flex-col items-start gap-8 text-center md:text-start">
+          <h3 className="text-blue-400 font-extrabold text-xs leading-normal tracking-tight uppercase mt-8 mx-auto md:mx-0">
             #1 SaaS website
           </h3>
           <div className="pb-2 w-full">
             <h1
-            className="md:text-[3rem] text-[1.1rem]"
               style={{
                 color: "var(--neutral-black, #000)",
+                fonz
                 fontStyle: "normal",
                 fontWeight: "700",
                 lineHeight: "normal",
               }}
             >
-              Elevate Your Business 
+              Elevate Your Business <br className="hidden md:block" />
               with{" "}
-              <span className="h-fit w-fit relative   z-0 ">
+              <span className="h-fit w-fit relative  z-0 ">
                 <span
                   style={{
                     color: "var(--main, #4255BD)",
@@ -31,7 +31,7 @@ const HomeSection = () => {
                     fontWeight: "700",
                     textTransform: "uppercase",
                   }}
-                  className="border border-solid border-[#4255BD] py-2 px-6 h-fit text-center"
+                  className="border border-solid border-[#4255BD] p-2 px-6 h-fit text-center"
                 >
                   Virtuex
                 </span>
