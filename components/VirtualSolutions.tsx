a0da511c778259c0e21eb35e0c051b7b562d226c
Iqtidartara <135112726+Iqtidartara@users.noreply.github.com>
2024-01-11 03:19:39 +0500

    refractor: puting animations

--- components/VirtualSolutions.tsx
+++ components/VirtualSolutions.tsx
@@ -28,14 +28,14 @@ const virtualSolutionsData = [
 
 function VirtualSolutions() {
   return (
-    <div className="p-5 md:px-10 md:py-12 flex flex-col justify-center items-center">
+    <div className="p-5 md:px-10 md:py-12 flex flex-col justify-center items-center max-w-[80rem]">
       <header className="text-indigo-500 text-xs font-black leading-[133.333%] tracking-wide uppercase self-center justify-center items-center  w-[83px] max-w-full px-2.5 py-1 rounded-[6.1875rem]">
         Features
       </header>
       <h1 className="text-zinc-800 text-center text-3xl md:text-4xl font-bold self-center max-w-[405px] mt-6">
         Powering Your Digital Transformation
       </h1>
-      <p className="text-zinc-800 text-center text-base mt-7 max-md:max-w-full [text-wrap:balance]" >
+      <p className="text-zinc-800 text-center text-base mt-7 [text-wrap:balance]" >
         Virtuex provides a comprehensive range of virtual solutions designed to enhance productivity, efficiency, and success for businesses across various industries. Our solutions include:
       </p>
       <div className="justify-center self-center  w-full max-w-[72rem] mt-12 max-md:max-w-full max-md:mt-10">
